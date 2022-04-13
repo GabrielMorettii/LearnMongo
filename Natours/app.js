e54ad648462f9635a1b242550c623b27c15fcd8e
GabrielMoretti <gabrielmorettisilva@gmail.com>
2022-04-13 14:17:20 -0300

    feature: Error Handling

--- Natours/app.js
+++ Natours/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
 
 const app = express();
 
@@ -17,5 +18,23 @@ app.use(express.static(`${__dirname}/public`));
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.use((req, res, next) => {
+  next(new AppError('The page was not found!', 404));
+});
+
+app.use((err, req, res, next) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: `Internal Server Error: ${err.message}`
+  });
+});
 
 module.exports = app;

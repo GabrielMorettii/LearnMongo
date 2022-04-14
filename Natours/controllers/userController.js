9246232c04fcae69927f18768b2dc1d796314053
GabrielMoretti <gabrielmorettisilva@gmail.com>
2022-04-14 13:32:56 -0300

    feature: Login

--- Natours/controllers/userController.js
+++ Natours/controllers/userController.js
@@ -1,7 +1,15 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
+const User = require('../models/userModel');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.getAllUsers = async (req, res) => {
+  const users = await User.find();
+
+  return res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users
+    }
   });
 };
 exports.getUser = (req, res) => {

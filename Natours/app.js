a233b93393e9ce01d6c92e7daf72a21d7ac84877
GabrielMoretti <gabrielmorettisilva@gmail.com>
2022-04-16 08:31:15 -0300

    feature: RateLimit + Helmet

--- Natours/app.js
+++ Natours/app.js
@@ -1,6 +1,8 @@
 require('express-async-errors');
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -8,11 +10,19 @@ const globalErrorHandler = require('./controllers/errorHandler');
 
 const app = express();
 
+app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: new AppError('Too many requests', 429)
+});
+
+app.use('/api', limiter);
+app.use(express.json({ limit: '10kb' }));
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {

6cbe35978fb4fbc9aebcd4fb3d9674590eb2bae1
GabrielMoretti <gabrielmorettisilva@gmail.com>
2022-04-17 18:55:01 -0300

    feature: AddingAverageTour

--- Natours/models/reviewModel.js
+++ Natours/models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -41,6 +42,30 @@ reviewSchema.pre(/^find/, function(next) {
   });
 
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function(tour) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+
+  await Tour.findByIdAndUpdate(tour, {
+    ratingsAverage: stats[0].avgRating,
+    ratingsQuantity: stats[0].nRating
+  });
+};
+
+reviewSchema.post('save', function() {
+  this.constructor.calcAverageRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
